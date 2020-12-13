80ad769f55a8d1919b32682a568cbee37a543fd4
p-raeis <75837847+p-raeis@users.noreply.github.com>
2020-12-14 01:16:15 +0330

    Add files via upload

---
+++ BLL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Selection.Data_Access_Layer;
+
+namespace Selection.Business_Logic_Layer
+{
+    public class BLL
+    {
+        DAL DataLayer;
+        public BLL()
+        {
+            DataLayer = new DAL();
+        }
+        public Students GetStudentByID(int id)
+        {
+            return DataLayer.GetStudentByID(id);
+        }
+        public Result Register(Students student)
+        {
+            Result error = new Result();
+            if (GetStudentByID(student.ID) == null)
+            {
+                error.Error = !DataLayer.RegisterStudent(student);
+            }
+            else
+            {
+
+                error.Error = true;
+                error.Message = "دانشجو با این شماره دانشجویی وجود دارد";
+            }
+
+            return error;
+        }
+        public List<Courses> GetCourses()
+        {
+            return DataLayer.GetCourses();
+        }
+        public Result CheckPassword(int StudentId, string Password)
+        {
+            Students student = DataLayer.GetStudentByID(StudentId);
+            Result error = new Result();
+            if (student != null)
+            {
+                if (student.Phone == Password)
+                    error.Error = false;
+                else
+                {
+                    error.Error = true;
+                    error.Message = "پسورد اشتباه است";
+                }
+                return error;
+            }
+            else
+            {
+                error.Error = true;
+                error.Message = "کاربری با این مشخصات یافت نشد";
+
+                return error;
+            }
+        }
+        public bool BindeCourse(int studentId, int CourseId)
+        {
+            TakenCourse takenCourses = new TakenCourse()
+            {
+                ID = studentId,
+                CoureseId = CourseId
+            };
+            if (CheackTakenCourse(takenCourses))
+                return false;
+            else
+                return DataLayer.BindCourse(takenCourses); 
+
+        }
+        public bool CheackTakenCourse(TakenCourse takenCourses)
+        {
+
+            return DataLayer.CheackTakenCourse(takenCourses);
+        }
+
+    }
+}
---
+++ DAL.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Selection.Data_Access_Layer
+{
+    public class DAL
+    {
+        SelectUnitEntities1 db;
+        public DAL()
+        {
+            db = new SelectUnitEntities1();
+        }
+        public bool RegisterStudent(Students student)
+        {
+
+            try
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        public Students GetStudentByID(int Id)
+        {
+            try
+            {
+                return db.Students.Where(x => x.ID == Id).SingleOrDefault();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public List<Courses> GetCourses()
+        {
+            try
+            {
+                return db.Courses.ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public bool BindCourse(TakenCourse takenCourses)
+        {
+            try
+            {
+                db.TakenCourse.Add(takenCourses);
+                db.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                var x = ex.Message;
+                return false;
+            }
+        }
+        public bool CheackTakenCourse(TakenCourse takenCourses)
+        {
+            try
+            {
+                return db.TakenCourse.Where(x=>x.CoureseId==takenCourses.CoureseId && x.ID==takenCourses.ID).FirstOrDefault() != null;
+            }
+            catch(Exception ex)
+            {
+                var x = ex.Message;
+                return false;
+            }
+        }
+    }
+}
