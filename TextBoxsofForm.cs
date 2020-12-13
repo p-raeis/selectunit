ba2d221662c5a3af5501ff78d0843631c34017c8
hakimzade99 <75672814+hakimzade99@users.noreply.github.com>
2020-12-13 22:30:24 +0330

    Add files via upload

---
+++ TextBoxsofForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selection
+{
+    static class TextBoxsofForm
+    {
+        public static bool IsNullOrEmpty(Form form)
+        {
+            foreach (Control control in form.Controls)
+            {
+                if (control is TextBox)
+                    if (string.IsNullOrEmpty((control as TextBox).Text))
+                        return true;
+            }
+            return false;
+        }
+    }
+}
