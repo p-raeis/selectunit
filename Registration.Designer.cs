6e655bdeb8ee1443d9b6360bb90c868c1bbeb037
p-raeis <75837847+p-raeis@users.noreply.github.com>
2020-12-14 17:20:01 +0330

    Add files via upload

---
+++ Registration.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Selection.User_Interface
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtField = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(55, 80);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtPhone.Size = new System.Drawing.Size(130, 22);
+            this.txtPhone.TabIndex = 7;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(427, 80);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtStudentID.Size = new System.Drawing.Size(161, 22);
+            this.txtStudentID.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(204, 76);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(84, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "شماره تماس:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(628, 78);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "شماره دانشجویی:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Location = new System.Drawing.Point(427, 172);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtFamily.Size = new System.Drawing.Size(161, 22);
+            this.txtFamily.TabIndex = 9;
+            this.txtFamily.TextChanged += new System.EventHandler(this.txtFamily_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(628, 168);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "نام خانوادگی:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(362, 220);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtAddress.Size = new System.Drawing.Size(226, 130);
+            this.txtAddress.TabIndex = 11;
+            this.txtAddress.TextChanged += new System.EventHandler(this.txtAddress_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(628, 217);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(45, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "آدرس:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(427, 125);
+            this.txtName.Name = "txtName";
+            this.txtName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtName.Size = new System.Drawing.Size(161, 22);
+            this.txtName.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(628, 119);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(29, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "نام:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtField
+            // 
+            this.txtField.Location = new System.Drawing.Point(55, 123);
+            this.txtField.Name = "txtField";
+            this.txtField.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtField.Size = new System.Drawing.Size(130, 22);
+            this.txtField.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(204, 119);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(61, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "کد رشته:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label7.Location = new System.Drawing.Point(204, 163);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(54, 18);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "جنسیت:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Location = new System.Drawing.Point(142, 168);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(43, 21);
+            this.Female.TabIndex = 17;
+            this.Female.TabStop = true;
+            this.Female.Text = "زن";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Location = new System.Drawing.Point(77, 166);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(46, 21);
+            this.Male.TabIndex = 18;
+            this.Male.TabStop = true;
+            this.Male.Text = "مرد";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnRegister.Location = new System.Drawing.Point(183, 293);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(126, 88);
+            this.btnRegister.TabIndex = 19;
+            this.btnRegister.Text = "ثبت نام عملیات انتخاب واحد";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.Location = new System.Drawing.Point(42, 293);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(125, 88);
+            this.btnCancel.TabIndex = 20;
+            this.btnCancel.Text = "انصراف ثبت اطلاعات دانشجو";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblError.Location = new System.Drawing.Point(149, 364);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 21;
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.Male);
+            this.Controls.Add(this.Female);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtField);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registration";
+            this.Text = "ثبت";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtField;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ Registration.cs
@@ -0,0 +1,70 @@
+﻿using Selection.Business_Logic_Layer;
+using Selection.Data_Access_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Selection.User_Interface
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            if (TextBoxsofForm.IsNullOrEmpty(this))
+            {
+                lblError.Text = "تمامی فیلد ها باید پر شوند";
+                return;
+            }
+            Students studentModel = new Students();
+            studentModel.Address = txtAddress.Text;
+            studentModel.Family = txtFamily.Text;
+            studentModel.FieldId = int.Parse(txtField.Text);
+            if (Female.Checked)
+                studentModel.Gender = false;
+            else
+                studentModel.Gender = true;
+            studentModel.ID = int.Parse(txtStudentID.Text);
+            studentModel.Name = txtName.Text;
+            studentModel.Phone = txtPhone.Text;
+
+
+            BLL BusinessLayer = new BLL();
+            Result result = BusinessLayer.Register(studentModel);
+            if (!result.Error)
+            {
+                MessageBox.Show("ثبت اطلاعات با موفقیت انجام  شد");
+            }
+            else
+            {
+                MessageBox.Show("ثبت انجام نشد" + result.Message);
+            }
+
+        }
+
+        private void txtAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFamily_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
